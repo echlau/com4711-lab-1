06710640100534c49fa738886dbf770a39762e7c
echlau <ericlau96@hotmail.com>
2016-09-08 17:26:32 -0700

    Made a third student and added comments to code

--- Student.php
+++ Student.php
@@ -12,7 +12,7 @@
  * @author Eric Lau
  */
 class Student {
-    //put your code here
+    //Constructor for Student Object
     function __construct() {
     $this->surname = '';
     $this->first_name = '';
@@ -20,14 +20,17 @@ class Student {
     $this->grades = array();
     }
     
+    //Adds an email to the student
     function add_email($which,$address) {
     $this->emails[$which] = $address;
     }
-
+    
+    //Adds a grade to the student
     function add_grade($grade) {
     $this->grades[] = $grade;
     }
     
+    //Averages the grades of the student
     function average() {
     $total = 0;
     foreach ($this->grades as $value)
@@ -35,11 +38,12 @@ class Student {
     return $total / count($this->grades);
     }
     
+    //Turns the data stored in the object into a string
     function toString() {
     $result = $this->first_name . ' ' . $this->surname;
     $result .= ' ('.$this->average().")\n";
     foreach($this->emails as $which=>$what)
-    $result .= $which . ': '. $what. "\n";
+    $result .= $which . ": \t". $what. "\n";
     $result .= "\n";
     return '<pre>'.$result.'</pre>';
     }
--- index.php
+++ index.php
@@ -12,9 +12,13 @@ and open the template in the editor.
     </head>
     <body>
         <?php
+        //Include student class
         include('student.php');
+        
+        //Creates an array to store students
         $students = array();
         
+        //First Student
         $first = new Student();
         $first->surname = "Doe";
         $first->first_name = "John";
@@ -25,6 +29,7 @@ and open the template in the editor.
         $first->add_grade(55);
         $students['j123'] = $first;
         
+        //Second Student
         $second = new Student();
         $second->surname = "Einstein";
         $second->first_name = "Albert";
@@ -35,7 +40,22 @@ and open the template in the editor.
         $second->add_grade(80);
         $second->add_grade(50);
         $students['a456'] = $second;
+        
+        //Third Student
+        $thrid = new Student();
+        $thrid->surname = "Lau";
+        $thrid->first_name = "Eric";
+        $thrid->add_email('home','testHome@email.com');
+        $thrid->add_email('work','fakeWork@email.com');
+        $thrid->add_grade(69);
+        $thrid->add_grade(42);
+        $thrid->add_grade(100);
+        $students['e666'] = $thrid;
 
+        //Sorts students in acending order by array key
+        ksort($students);
+        
+        //Prints information of each student in $students array
         foreach($students as $student)
         echo $student->toString();
 
